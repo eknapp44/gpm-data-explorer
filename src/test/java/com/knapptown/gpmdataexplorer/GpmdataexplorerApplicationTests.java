c356422ed78c85a43fd5cb74d3b01cff0a57613e
Evan <pnkme45@gmail.com>
2021-01-03 22:06:23 -0600

    Excluding tests from the CI temporarily.

--- src/test/java/com/knapptown/gpmdataexplorer/GpmdataexplorerApplicationTests.java
+++ src/test/java/com/knapptown/gpmdataexplorer/GpmdataexplorerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.knapptown.gpmdataexplorer;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class GpmdataexplorerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
