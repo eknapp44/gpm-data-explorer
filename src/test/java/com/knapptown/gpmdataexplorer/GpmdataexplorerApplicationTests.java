d6d0e4a8a5d1c75e8e4414c9ffa6ea466c11b2b5
Evan <pnkme45@gmail.com>
2021-01-04 21:07:13 -0600

    Renabling tests and disabling the boot test.

--- src/test/java/com/knapptown/gpmdataexplorer/GpmdataexplorerApplicationTests.java
+++ src/test/java/com/knapptown/gpmdataexplorer/GpmdataexplorerApplicationTests.java
@@ -3,9 +3,9 @@ package com.knapptown.gpmdataexplorer;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GpmdataexplorerApplicationTests {
 
 	@Test
